5f62f5deff9c9168d522a1051e6a924d5eb59948
Igor <igor.mamaev1@gmail.com>
2025-11-13 09:55:13 +0300

    Возможность редактировать роли

--- bot/handlers/editrole.py
+++ bot/handlers/editrole.py
@@ -9,9 +9,13 @@ from aiogram_dialog.widgets.text import Const, Format
 from aiogram_dialog.widgets.kbd import Button, Row, Column, Cancel, Url
 from aiogram_dialog.widgets.kbd import ScrollingGroup, Select
 from sqlalchemy import select
+from sqlalchemy.orm.sync import update
+from sqlalchemy.orm import selectinload
+
 
 from bot.models import Song, SongParticipation, Person
 from bot.services.database import get_db_session
+from bot.services.role import is_valid_role
 from bot.services.settings import settings
 from bot.services.url import parse_url
 from bot.states.editrole import EditRole
@@ -20,32 +24,63 @@ router = Router()
 
 
 async def main_getter(dialog_manager: DialogManager, **kwargs) -> dict:
+    pid = int(dialog_manager.start_data["participation_id"])
+
     async with get_db_session() as session:
-        participation: SongParticipation = (
+        participation = (
             await session.execute(
-                select(SongParticipation).where(
-                    SongParticipation.id
-                    == int(dialog_manager.start_data["participation_id"])
+                select(SongParticipation)
+                .where(SongParticipation.id == pid)
+                .options(
+                    selectinload(SongParticipation.person),
+                    selectinload(SongParticipation.song),
                 )
             )
         ).scalar_one_or_none()
-        person: Person = (
-            await session.execute(
-                select(Person).where(Person.id == participation.person_id)
-            )
-        ).scalar_one_or_none()
-        song: Song = (
-            await session.execute(
-                select(Song).where(Song.id == participation.song_id)
-            )
-        ).scalar_one_or_none()
+
     return {
-        "participation_id": dialog_manager.start_data["participation_id"],
-        "person_id": person.id,
-        "person_name": person.name,
-        "song_title": song.title,
+        "participation_id": pid,
+        "person_id": participation.person.id,
+        "person_name": participation.person.name,
+        "song_title": participation.song.title,
         "role": participation.role,
     }
+
+
+from sqlalchemy import update
+
+
+async def on_role_input(
+    message: Message, msg_input: MessageInput, dialog_manager: DialogManager
+):
+    new_role = message.text
+
+    if not is_valid_role(new_role):
+        return
+
+    participation_id = int(dialog_manager.start_data["participation_id"])
+
+    async with get_db_session() as session:
+        await session.execute(
+            update(SongParticipation)
+            .where(SongParticipation.id == participation_id)
+            .values(role=new_role)
+        )
+        await session.commit()
+
+        participation: SongParticipation = (
+            await session.execute(
+                select(SongParticipation)
+                .where(SongParticipation.id == participation_id)
+                .options(selectinload(SongParticipation.song))
+            )
+        ).scalar_one_or_none()
+        await message.bot.send_message(
+            chat_id=participation.person_id,
+            text=f"{message.from_user.mention_html()} изменил ваше участие в песне <b>{participation.song.title}</b> на <b>{new_role}</b>",
+        )
+
+    await dialog_manager.switch_to(EditRole.menu)
 
 
 router.include_router(
@@ -57,9 +92,29 @@ router.include_router(
             Url(
                 Const("Перейти в профиль"), Format("tg://user?id={person_id}")
             ),
+            Button(
+                Const("Редактировать название роли"),
+                id="edit_role",
+                on_click=lambda c, b, m: m.switch_to(EditRole.input_role),
+            ),
+            Button(
+                Const("Удалить"),
+                id="remove_role",
+                on_click=lambda c, b, m: c.answer("TODO"),
+            ),
             Cancel(Const("Назад")),
             getter=main_getter,
             state=EditRole.menu,
-        )
+        ),
+        Window(
+            Const("Введи новое название роли"),
+            MessageInput(content_types=ContentType.TEXT, func=on_role_input),
+            Button(
+                Const("Назад"),
+                id="back",
+                on_click=lambda c, b, m: m.switch_to(EditRole.menu),
+            ),
+            state=EditRole.input_role,
+        ),
     )
 )
--- bot/states/editrole.py
+++ bot/states/editrole.py
@@ -3,3 +3,5 @@ from aiogram.fsm.state import StatesGroup, State
 
 class EditRole(StatesGroup):
     menu = State()
+    remove_confirm = State()
+    input_role = State()
