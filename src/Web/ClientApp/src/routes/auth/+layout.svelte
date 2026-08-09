618ead89c3db46982f9039359dda7a6f9a4bf680
Igor Mamaev <igor.mamaev1@gmail.com>
2026-08-09 23:27:05 +0300

    дизайн экрана логина

--- src/Web/ClientApp/src/routes/auth/+layout.svelte
+++ src/Web/ClientApp/src/routes/auth/+layout.svelte
@@ -4,26 +4,13 @@
     let { data, children }: LayoutProps = $props();
 </script>
 
-<div class="grid min-h-svh lg:grid-cols-2">
-    <div class="flex flex-col gap-4 p-6 md:p-10">
-<!--        <div class="flex justify-center gap-2 md:justify-start">-->
-<!--            <a href="https://nornickel.ru" class="flex items-center gap-2 font-medium">-->
-<!--                <div class="z-logo header__logo header__logo&#45;&#45;big z-logo&#45;&#45;full-image"><img-->
-<!--                        src="https://nornickel.ru/images/logo/logo-inverted-ru.svg" alt="logo" class="z-logo__img">-->
-<!--                </div>-->
-<!--            </a>-->
-<!--        </div>-->
-        <div class="flex flex-1 items-center justify-center">
-            <div class="w-full max-w-xs">
-                {@render children()}
-            </div>
-        </div>
-    </div>
-    <div class="bg-muted relative hidden lg:block">
-        <img
-                src={data.bgImage}
-                alt=""
-                class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+<div class="relative flex min-h-svh items-center justify-center overflow-hidden p-6 md:p-10">
+    <img
+            src={data.bgImage}
+            alt=""
+            class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+    />
+    <div class="relative z-10 w-full max-w-xs">
+        {@render children()}
     </div>
 </div>
