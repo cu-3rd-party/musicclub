19407475a096722e69ec8ade6790adbfdbe2afa5
Igor Mamaev <igor.mamaev1@gmail.com>
2026-08-30 19:36:04 +0300

    use env.API_SSR_URL to fetch images

--- src/Web/ClientApp/src/routes/data/[...path]/+server.ts
+++ src/Web/ClientApp/src/routes/data/[...path]/+server.ts
@@ -1,9 +1,9 @@
-import { API_URL } from "$lib/config";
+import { env } from "$env/dynamic/private";
 import type { RequestHandler } from "./$types";
 
 // может быть это добавляет х2 к лейтенси и к загрузке сети, но как будто похуй
 export const GET: RequestHandler = async ({ params, url, fetch }) => {
-    const target = new URL(`/api/v1/data/${params.path}${url.search}`, API_URL);
+    const target = new URL(`/api/v1/data/${params.path}${url.search}`, env.API_SSR_URL);
 
     const response = await fetch(target);
 
